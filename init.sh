37f9309a100297dcf9ad87743c8dea811c1639e9
mxsl-gr <mxsl.gr@gmail.com>
2016-12-10 12:45:19 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,17 +4,22 @@ echo "install homebrew..."
 echo "install oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-echo "install google-chrome sourcetree docker kitematic..."
-brew cask install google-chrome sourcetree docker kitematic
-
 echo "install java go nvm..."
 brew cask install java go nvm
 
-echo "install intellij-idea-ce visual-studio-code..."
-brew cask install intellij-idea-ce visual-studio-code
+# set nvm
+mkdir ~/.nvm
+export NVM_DIR="$HOME/.nvm" >> ~/.zshrc
+. "/usr/local/opt/nvm/nvm.sh" >> ~/.zshrc
 
 echo "install maven git..."
 brew cask maven git
+
+echo "install google-chrome sourcetree docker kitematic..."
+brew cask install google-chrome sourcetree docker kitematic
+
+echo "install intellij-idea-ce visual-studio-code..."
+brew cask install intellij-idea-ce visual-studio-code
 
 echo "install qq neteasemusic..."
 brew cask install qq neteasemusic
